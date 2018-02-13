926c34bd2c258a2fd5911742acbca87d13a7b33f
markpeou <markpeou@gmail.com>
2018-02-13 15:32:18 +1100

    updated changes

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 console.log('boop');
 
+$(function() {
+  $('.box').hide();
+});
+
 $('button').on('click',function(event) {
 event.preventDefault();
 var searchInput = $('#search').val()
 var $brush = $('.brush');
 $brush.css("background-color",searchInput);
+$('.box').show();
 
+$(document).ready(function () {
+    var $newdiv = $('<div class="squares" />');
+    for (var i = 0; i < 100; i++) {
+        $newdiv = $('<div class="squares" />').css("background-color",searchInput);
+        $('.box').append($newdiv);
+
+    }
+  });
 });
